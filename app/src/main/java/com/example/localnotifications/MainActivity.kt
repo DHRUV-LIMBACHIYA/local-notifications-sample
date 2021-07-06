b0d5a196eb56f6b1c5fc340b6a710eee267d88d0
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-07-06 19:55:06 +0530

    Created a simple notification.

--- app/src/main/java/com/example/localnotifications/MainActivity.kt
+++ app/src/main/java/com/example/localnotifications/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.localnotifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.localnotifications.databinding.ActivityMainBinding
+import com.example.localnotifications.util.NotificationUtil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var notificationUtil: NotificationUtil
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        notificationUtil = NotificationUtil(this)
+
+        notificationUtil.createNotificationChannel() // Create a notification channel for Android 8 or higher
+        notificationUtil.buildNotification() // Build a notification.
+
+        mBinding.fireNotificationButton.setOnClickListener {
+            notificationUtil.displayNotification() // Fire a notification.
+        }
     }
 }
