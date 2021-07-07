86b7af73cbeff507d25ac70945fa379d613c3112
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-07-07 11:27:17 +0530

    Added action buttons in notification

--- app/src/main/java/com/example/localnotifications/MainActivity.kt
+++ app/src/main/java/com/example/localnotifications/MainActivity.kt
@@ -7,7 +7,6 @@ import com.example.localnotifications.util.NotificationUtil
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var notificationUtil: NotificationUtil
     private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,13 +14,17 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        notificationUtil = NotificationUtil(this)
 
-        notificationUtil.createNotificationChannel() // Create a notification channel for Android 8 or higher
-        notificationUtil.buildNotification() // Build a notification.
+        NotificationUtil.createNotificationChannel(this) // Create a notification channel for Android 8 or higher
+        NotificationUtil.buildNotificationWithActionButtons(this) // Build a notification with action button.
 
         mBinding.fireNotificationButton.setOnClickListener {
-            notificationUtil.displayNotification() // Fire a notification.
+            NotificationUtil.displayNotification(this) // Fire a notification.
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        NotificationUtil.clearRes()
     }
 }
--- app/src/main/java/com/example/localnotifications/util/NotificationUtl.kt
+++ app/src/main/java/com/example/localnotifications/util/NotificationUtl.kt
@@ -1,5 +1,6 @@
 package com.example.localnotifications.util
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -9,6 +10,7 @@ import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.example.localnotifications.NotificationResponseActivity
 import com.example.localnotifications.R
+import com.example.localnotifications.actionhandler.NotificationActionIntentService
 
 /**
  * Created by Dhruv Limbachiya on 06-07-2021.
@@ -18,27 +20,27 @@ import com.example.localnotifications.R
 // Step 2 : Create a Notification Channel for Android 8.0 or higher.
 // Step 3: Display Notification.
 
-const val NOTIFICATION_CHANNEL_ID = "CHANNEL_ID_ONE"
-const val NOTIFICATION_ID = 101
 
-class NotificationUtil(private val context: Context) {
+object NotificationUtil {
 
-    private lateinit var notificationManager: NotificationManager
+    const val NOTIFICATION_CHANNEL_ID = "CHANNEL_ID_ONE"
+    const val NOTIFICATION_ID = 101
+
+    @SuppressLint("StaticFieldLeak")
     private var notificationBuilder: NotificationCompat.Builder? = null
-
 
     /**
      * Step 1 : Build Notification using NotificationCompat.Builder
      * Function responsible for building Notification.
      */
-    fun buildNotification() {
+    fun buildNotification(context: Context) {
 
-        val intent = Intent(context,NotificationResponseActivity::class.java).apply {
+        val intent = Intent(context, NotificationResponseActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
 
         // Pending Intent to open a new activity in future.
-        val pendingIntent = PendingIntent.getActivity(context,0,intent,0)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
         notificationBuilder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_directions_bike_24) // Display a small icon on the left side.
@@ -52,7 +54,7 @@ class NotificationUtil(private val context: Context) {
     /**
      * Step 2 : Create a Notification Channel for Android 8.0 or higher.
      */
-    fun createNotificationChannel() {
+    fun createNotificationChannel(context: Context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelName = context.getString(R.string.channel_number_one) // Channel name
             val channelDescription =
@@ -65,17 +67,71 @@ class NotificationUtil(private val context: Context) {
                 }
 
             // Register the channel with system.
-            notificationManager =
-                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(notificationChannel)
+            getNotificationManager(context).createNotificationChannel(notificationChannel)
         }
     }
 
     /**
      * Step 3: Display Notification.
      */
-    fun displayNotification() {
-        notificationManager.notify(NOTIFICATION_ID, notificationBuilder?.build())
+    fun displayNotification(context: Context) {
+        getNotificationManager(context).notify(NOTIFICATION_ID, notificationBuilder?.build())
     }
+
+    fun buildNotificationWithActionButtons(context: Context) : NotificationCompat.Builder?{
+        // Snooze Action
+        val snoozeIntent = Intent(context, NotificationActionIntentService::class.java).apply {
+            action = NotificationActionIntentService.SNOOZE_ACTION
+        }
+
+        // Dismiss Action
+        val dismissIntent = Intent(context, NotificationActionIntentService::class.java).apply {
+            action = NotificationActionIntentService.DISMISS_ACTION
+        }
+
+        val snoozePendingIntent = PendingIntent.getService(context, 0, snoozeIntent, 0)
+        val dismissPendingIntent = PendingIntent.getService(context, 0, dismissIntent, 0)
+
+        notificationBuilder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_directions_bike_24) // Display a small icon on the left side.
+            .setContentTitle("Cycling") // Notification Title
+            .setContentText("Let take a ride") // Notification Subtitle.
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT) // Set the interrupting behaviour by giving priority.
+            .addAction(
+                NotificationCompat.Action(
+                    R.drawable.ic_baseline_alarm_24,
+                    "Snooze",
+                    snoozePendingIntent
+                )
+            ) // Add Snooze action button
+            .addAction(
+                NotificationCompat.Action(
+                    R.drawable.ic_baseline_cancel_24,
+                    "Dismiss",
+                    dismissPendingIntent
+                )
+            ) // Add Dismiss action button.
+            .setAutoCancel(true) // Dismiss/Cancel the notification on Tap.
+
+        setNotificationBuilderInstance(notificationBuilder!!)
+
+        return notificationBuilder
+    }
+
+
+    fun getNotificationManager(context: Context): NotificationManager {
+        return context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+
+    private fun setNotificationBuilderInstance(builder: NotificationCompat.Builder) {
+        this.notificationBuilder = builder
+    }
+
+    fun getNotificationBuilder(): NotificationCompat.Builder? = notificationBuilder
+
+    fun clearRes() {
+        notificationBuilder = null
+    }
+
 
 }
